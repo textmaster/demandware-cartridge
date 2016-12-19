15d942afcae70b1426dc433f5142efea44df2193
Pradeep Paraperuthady <pradeep.p@ideatarmac.com>
2016-12-19 15:15:38 +0530

    initial cartridge commit

---
+++ bm_textmaster/cartridge/scripts/util/PipelinesMap.js
@@ -0,0 +1,21 @@
+/**
+ * This is a Key:Value map of Pipeline names. Get the Pipeline name using its key. */
+
+module.exports = {
+		"export-data-product":"ProductsExport-Products",
+		"export-data-product-url":"ProductsExport-ProductUrls",
+		"export-data-product-image":"ProductsExport-ProductImages",
+		"export-data-product-price":"PricebookExport-ProductNetPrice",
+		"export-data-catalog":"StandardExport-Catalog",
+		"export-data-pricebook":"StandardExport-PriceBook",
+		"export-data-inventory":"StandardExport-Inventory",
+		"export-meta-catalog":"MetaExport-CatalogMeta",
+		"export-meta-inventory":"MetaExport-InventoryMeta",
+		"export-meta-pricebook":"MetaExport-PricebookMeta",
+		"export-meta-category":"MetaExport-CategoryMeta",
+		"export-meta-customattribute":"MetaExport-CustomAttributeMeta",
+		"import-data-inventory":"StandardImport-Inventory",
+		"import-data-catalog":"StandardImport-Catalog",
+		"import-data-product":"StandardImport-Catalog",
+		"import-data-pricebook":"StandardImport-PriceBook"
+};
