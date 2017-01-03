4be2693f4bfa88b9f4f984a43539e8badada8b96
Pradeep Paraperuthady <pradeep.p@ideatarmac.com>
2017-01-03 12:31:28 +0530

    Search button and load item list

--- bm_textmaster/cartridge/static/default/js/app.js
+++ bm_textmaster/cartridge/static/default/js/app.js
@@ -6,7 +6,7 @@
 				}
 			},
 			urls:{
-				categoryList: "Translation-CategoryList",
+				categoryDropdown: "Translation-CategoryDropdown",
 				translationItemList: "Translation-ItemList"
 			},
 			newTranslation: function(){
@@ -21,23 +21,36 @@
 				
 				$('select[name=item-type]').on('change',function(){
 					item = this.value;
-					if($(this).val() == "product"){
+					if($(this).val() == "product" || $(this).val() == "category"){
 						$('.field-holder.catalog-list').addClass("show");
+						$('.filter-items .category').removeClass('hide');
 					}
 					else{
 						$('.field-holder.catalog-list').removeClass("show");
 						$("select[name=catalog] option[value='']").prop("selected",true);
+						$('.filter-items .category').addClass('hide');
 					}
+					
+					if($(this).val() == "category"){
+						$('.filter-items .search-status').addClass('hide');
+					}
+					else{
+						$('.filter-items .search-status').removeClass('hide');
+					}
+					
 					$('.attributes-main').removeClass("product").removeClass("category").removeClass("content");
 					$('.attributes-main').addClass(item);
+					$(this).removeClass('error-field');
+					$('#items-holder').html('');
 				});
 				
 				$('select[name=catalog]').on('change',function(){
 					catalog = $(this).val();
-					url = app.urls.categoryList + "?catalog=" + catalog;
+					url = app.urls.categoryDropdown + "?catalog=" + catalog;
 					$.get(url, function(data){
 						$('select[name=filter-category]').html(data);
 					});
+					$(this).removeClass('error-field');
 				});
 				
 				$('#filter-search').on("click", function(){
@@ -48,6 +61,7 @@
 						searchStatus = $('select[name=filter-search-status]').val(),
 						category = $('select[name=filter-category]').val(),
 						translatedFlag = $('select[name=filter-translated]').val(),
+						error = false,
 						postData = {
 							itemType: itemType,
 							catalog: catalog,
@@ -57,6 +71,21 @@
 							translatedFlag: translatedFlag
 						};
 					
+					if(itemType == ""){
+						$('select[name=item-type]').addClass('error-field');
+						error = true;
+					}
+					else if(itemType != "content" && catalog == ""){
+						$('select[name=catalog]').addClass('error-field');
+						error = true;
+					}
+					
+					if(error){
+						$('.common-error.search').addClass('show');
+						return false;
+					}
+					
+					$('.common-error.search').removeClass('show');
 					$(this).prop("disabled",true).val("Please wait...");
 					$.post(app.urls.translationItemList, postData, function(data){
 						$('#items-holder').html(data);
