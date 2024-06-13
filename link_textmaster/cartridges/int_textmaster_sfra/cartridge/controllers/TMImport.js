c5d704c968058294ce86093b9936490c67813385
Pradeep Paraperuthady <pradeeperiya@gmail.com>
2024-06-13 17:43:15 +0530

    Import callback is generalised
    Callback URLs are now adding to document while creating documents

--- link_textmaster/cartridges/int_textmaster_sfra/cartridge/controllers/TMImport.js
+++ link_textmaster/cartridges/int_textmaster_sfra/cartridge/controllers/TMImport.js
@@ -13,26 +13,52 @@ var logUtils = require('*/cartridge/scripts/utils/tmLogUtils');
 var log = logUtils.getLogger('ImportCallback');
 
 /**
- * Calls start method
+ * Import callback controller method
  */
 server.post('Data', function (req, res, next) {
+    log.debug('Import callback received');
     var projectid = req.querystring.projectid;
     var documentid = req.querystring.documentid;
     var isFirstImport = req.querystring.isFirstImport ? req.querystring.isFirstImport : true;
 
-    var input = {
-        ProjectID: projectid,
-        DocumentID: documentid,
-        isFirstImport: isFirstImport
-    };
-    log.debug('Import callback received for Project: ' + projectid + ' | Document: ' + documentid);
+    if (!projectid || !documentid) {
+        var document = {};
 
-    var importData = require('*/cartridge/scripts/translation/importData');
-    var output = importData.output(input);
+        if (req && req.body) {
+            try {
+                document = JSON.parse(req.body);
+            } catch (e) {
+                log.error('Malformed Request body');
+            }
+        } else {
+            log.error('Request body is not found');
+        }
 
-    res.json(output);
+        projectid = document.project_id ? document.project_id : null;
+        documentid = document.id ? document.id : null;
+    }
 
-    next();
+    if (projectid && documentid) {
+        var input = {
+            ProjectID: projectid,
+            DocumentID: documentid,
+            isFirstImport: isFirstImport
+        };
+        log.debug('Project: ' + projectid + ' | Document: ' + documentid);
+
+        var importData = require('*/cartridge/scripts/translation/importData');
+        var output = importData.output(input);
+
+        res.json(output);
+
+        next();
+    } else {
+        log.debug('Invalid import callback');
+        res.setStatusCode(422);
+        res.json({ status: 'ERROR', success: false, error: true });
+
+        next();
+    }
 });
 
 module.exports = server.exports();
